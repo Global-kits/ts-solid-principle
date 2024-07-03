1f8fb71c41b5ed442ca624b0d8fecc3bd0756fd4
kaungkhantzawdev <kaungkhantzaw.dev@gmail.com>
2024-07-03 22:50:06 +0700

    add service actuall testing

--- src/services/test.service.test.ts
+++ src/services/test.service.test.ts
@@ -1,8 +1,30 @@
+import TestModel from "../models/test.model";
+import { TestRepo } from "../models/repositories/test.repo"
+import { TestService } from "./test.service"
+import mongoose from "mongoose";
+
 describe('Test service', () => {
-    it('should return a string', () => {
-        expect('hi').toBe('hi')
+    let testRepo: TestRepo;
+    let testService: TestService;
+
+    beforeEach(async () => {
+        await mongoose.connect("mongodb://localhost:27017/test")
+                  .then()
+                  .catch(err => console.log('Database connection error',err))
+        
+        testRepo = new TestRepo(TestModel);
+        testService = new TestService(testRepo);
     })
-    test('should return', () => {
-        expect('hi').toBe('hi')
+
+    afterAll(async() => {
+       await mongoose.connection.close()
+    })
+
+
+
+    test('should return', async() => {
+
+        const result = await testService.get()
+        expect(result).toStrictEqual([])
     })
 })
